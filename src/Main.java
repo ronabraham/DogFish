02c61aa10cb7719db38511fcf6cc6eacfc0af540
Ron <ron@rajyaseva.in>
2018-02-01 17:56:04 +0530

    Create Main.java
    recreating folder structure

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.*;
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame window = new JFrame();
+		PlayingSquare playingSquare = new PlayingSquare();
+		window.setSize(640,663);
+		window.setLocation(1000, 50);
+		window.setTitle("DogFish App");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.add(playingSquare);
+		window.setVisible(true);
+		window.setBackground(Color.BLUE);
+		
+		
+	}
+
+	/* (non-Java-doc)
+	 * @see java.lang.Object#Object()
+	 */
+	public Main() {
+		super();
+	}
+
+}
